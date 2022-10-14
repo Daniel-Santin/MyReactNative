c683f575b27e1e316d9008a3949c5c85b0373637
Daniel-Santin <32037139@mackenzista.com.br>
2022-10-14 15:01:37 -0300

    x

--- src/pages/Metas/Criar.js
+++ src/pages/Metas/Criar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View,Text,StyleSheet,Image,TextInput,TouchableOpacity } from "react-native";
 
-export default function Metas(){
+export default function Criar(){
 return(
     <View>
         
